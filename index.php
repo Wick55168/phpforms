176f2499b471f8692586129a115c0d97955b609b
Wick55168 <132609998+Wick55168@users.noreply.github.com>
2023-05-05 08:40:30 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+
+header('Access-Control-Allow-Methods: GET, POST');
+
+header("Access-Control-Allow-Headers: X-Requested-With");
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Retrieve all submitted fields
     $name = $_POST["name"];
